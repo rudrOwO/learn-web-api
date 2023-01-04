9a9c0c2b8e22e7c809f6d8afe8072f29aba80c35
Dardaul Hoque <bsse1126@iit.du.ac.bd>
2023-01-04 07:56:57 +0600

    scaffold sqlite database

---
+++ MiniFB.Core/Data/MiniFbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MiniFB.Core.Db;
+
+public partial class MiniFbContext : DbContext
+{
+    public MiniFbContext()
+    {
+    }
+
+    public MiniFbContext(DbContextOptions<MiniFbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=Data/MiniFB.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
